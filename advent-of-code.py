65e36b516683d899ec2717162aa7992a61c41b8c
biswaone <biswaprakash444@gmail.com>
2023-01-08 18:33:11 +0530

    modified the code in advent-of-code-1 to close the file after it is read

--- advent-of-code.py
+++ advent-of-code.py
@@ -1,17 +1,19 @@
-with open('calories.txt','r') as f:
-    calorie_count = []
-    sum_of_calories = 0
+with open('advent-of-code.txt','r') as f:
     lines = f.read()
     my_array = lines.split("\n")
 
-    for num in my_array:
-        if num != '':
-            sum_of_calories += int(num)
-        else:
-            calorie_count.append(sum_of_calories)
-            sum_of_calories = 0
-    print(max(calorie_count))
-    print(sum(list(reversed(sorted(calorie_count)))[0:3]))
+
+calorie_count = []
+sum_of_calories = 0
+
+for num in my_array:
+    if num != '':
+        sum_of_calories += int(num)
+    else:
+        calorie_count.append(sum_of_calories)
+        sum_of_calories = 0
+print(max(calorie_count))
+print(sum(list(reversed(sorted(calorie_count)))[0:3]))
 
     
     
